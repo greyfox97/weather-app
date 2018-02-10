860d6b6158fa44b3042154237889870f202e3577
deepak <beb@deepak.com>
2018-02-10 17:32:20 +0530

    weather app

--- weather.js
+++ weather.js
@@ -1,22 +1,40 @@
 $(document).ready(function(){
-  var lat,lon;
+  var lat,long;
+
+  //function to show loading that ajax request is going on
+  var showLoading = function(selector){
+    var html = "<div class='text-center'>";
+    html += "<img src='images/ajax-loader(1).gif'></img></div>";
+    selector.innerHTML=html;
+  }
+  //end of loading function
 
   if (navigator.geolocation) {
      navigator.geolocation.getCurrentPosition(function(position) {
        lat = position.coords.latitude;
        long = position.coords.longitude;
 
+  var selector = document.querySelector("#loading");
+  showLoading(selector);
+
   var url = 'http://ip-api.com/json';
   $.getJSON(url,function(data){
-    console.log(data);
-    $('#location').html(data.city + ',' + data.country + ',' + data.countryCode);
-  });
 
   url = 'https://cors-anywhere.herokuapp.com/' + 'https://api.darksky.net/forecast/4fa59bc637d52f0ffc6612c294109040/' + lat + ',' + long;
   $.getJSON(url,function(result){
+
+    console.log(data);
+    var x = "<div class='text-center'>";
+    x += data.city + ',' + data.country + ',' + data.countryCode + "</div>";
+    //$('#location').html(data.city + ',' + data.country + ',' + data.countryCode);
+
     console.log(result);
-    $('#weather').html(((result.currently.temperature-32)*5/9).toFixed(2) + ' &#8451;');
-    $('#summary').html(result.currently.summary);
+
+    x += "<div class='text-center'>" + ((result.currently.temperature-32)*5/9).toFixed(2) + ' &#8451;' + "</div>";
+    //$('#weather').html(((result.currently.temperature-32)*5/9).toFixed(2) + ' &#8451;');
+    x += "<div class='text-center'>" + result.currently.summary + "</div>";
+    //$('#summary').html(result.currently.summary);
+    selector.innerHTML=x;
     function getIcon(info) {
               switch (info){
                 case "clear-day":
@@ -45,7 +63,9 @@ $(document).ready(function(){
     var skycons = new Skycons({"color":"white"});
         skycons.add("icons", getIcon(result.currently.icon));
         skycons.play();
-  });
+
+        });
+      });
     });
   }
 });
